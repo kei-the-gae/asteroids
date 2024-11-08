b0bb641cfeeb640a874e661a77317f7994e54fcf
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-11-07 16:12:06 -0800

    add splitting asteroids if not min radius when colliding with player shots

--- asteroid.py
+++ asteroid.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 from constants import *
 from circleshape import CircleShape
@@ -11,3 +12,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius > ASTEROID_MIN_RADIUS:
+            random_angle = random.uniform(20, 50)
+
+            a = self.velocity.rotate(random_angle)
+            b = self.velocity.rotate(-random_angle)
+
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid.velocity = a * 1.2
+            asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid.velocity = b * 1.2
